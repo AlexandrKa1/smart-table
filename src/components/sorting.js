891084c53e74b1cd1bf54ad8893b973b1fbff7d7
Alexandr Kapralov <kaovalex@gmail.com>
2025-12-10 12:58:04 +0300

    first version

--- src/components/sorting.js
+++ src/components/sorting.js
@@ -1,18 +1,35 @@
-import {sortCollection, sortMap} from "../lib/sort.js";
+import { sortCollection, sortMap } from "../lib/sort.js";
 
 export function initSorting(columns) {
     return (data, state, action) => {
         let field = null;
         let order = null;
 
-        if (action && action.name === 'sort') {
-            // @todo: #3.1 — запомнить выбранный режим сортировки
+        // @todo: #3.1 и #3.2 — обработка нажатия на кнопку сортировки
+        if (action && columns.includes(action)) {
+            // Обновляем состояние нажатой кнопки
+            action.dataset.value = sortMap[action.dataset.value];
+            field = action.dataset.field;
+            order = action.dataset.value;
 
-            // @todo: #3.2 — сбросить сортировки остальных колонок
-        } else {
-            // @todo: #3.3 — получить выбранный режим сортировки
+            // Сбрасываем все остальные кнопки
+            columns.forEach(column => {
+                if (column !== action) {
+                    column.dataset.value = 'none';
+                }
+            });
+        }
+
+        // @todo: #3.3 — восстановление текущей сортировки при перерисовке
+        if (field === null && order === null) {
+            columns.forEach(column => {
+                if (column.dataset.value !== 'none') {
+                    field = column.dataset.field;
+                    order = column.dataset.value;
+                }
+            });
         }
 
         return sortCollection(data, field, order);
-    }
+    };
 }
